4670ba2213252ee8594e7d4be1a63d33fd1ea8d8
Lets21 <leito.t210403@outlook.es>
2025-01-25 15:12:43 -0500

    Prueba2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mincore.wsgi
+web: python manage.py migrate && python manage.py collectstatic --no-input && gunicorn mincore.wsgi
--- mincore/settings.py
+++ mincore/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-689jm%w=0*62@v&5ta&uq&g^2je=dbi_m%!6lufg67j420dw40'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-689jm%w=0*62@v&5ta&uq&g^2je=dbi_m%!6lufg67j420dw40')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
@@ -83,7 +82,7 @@ WSGI_APPLICATION = 'mincore.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
@@ -124,10 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
