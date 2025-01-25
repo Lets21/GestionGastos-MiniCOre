0c6f51fb2feb306c92373fbb9e110c9e8237f354
Lets21 <leito.t210403@outlook.es>
2025-01-25 15:41:57 -0500

    Configure 7

--- mincore/settings.py
+++ mincore/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-689jm%w=0*62@v&5ta&uq&g^2je=dbi_m%!6lufg67j420dw40')
+SECRET_KEY = os.environ.get('SECRET_KEY', '&^3f+=3hf22w6_!$mx^3lqspvdiauh_b^d@%hf=dfn5nj1c#j5')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
